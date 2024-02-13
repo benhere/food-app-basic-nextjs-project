e2df3344763f3cdbe005f47d30789cfcfa1e9054
benhere <benjamin.oldfriend@gmail.com>
2024-02-13 19:24:32 +0530

    basic-nextjs-app_1.1

---
+++ src/app/meals/[mealSlug]/page.js
@@ -0,0 +1,8 @@
+
+
+
+export default function MealDetailsPage(){
+    return(
+        <h1>Meal Details Page</h1>
+    )
+}
---
+++ src/app/meals/share/page.js
@@ -0,0 +1,6 @@
+
+export default function ShareMealPage(){
+    return(
+        <h1>Share Meal Page</h1>
+    )
+}
