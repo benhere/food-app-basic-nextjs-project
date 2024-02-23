21c7cc39b12285a979770acc368556f673dba019
benhere <benjamin.oldfriend@gmail.com>
2024-02-23 16:40:17 +0530

    basic-nextjs-app_1.5

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,7 +1,28 @@
+import { getMeals } from '../../../lib/meals-util';
+import MealsGrid from '../components/meals/meals-grid';
+import classes from './page.module.css'
+import Link from 'next/link';
 
-export default function MealsPage(){
-    console.log('Executing meals page...');
-    return(
-        <h1>Meals Page</h1>
-    )
+export default async function MealsPage(){
+    // console.log('Executing meals page...');
+
+    const meals = await getMeals();
+    return (
+      <>
+        <header className={classes.header}>
+          <h1>
+            Delicious meals, created <span className={classes.highlight}>by you</span>
+          </h1>
+          <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+          <p className={classes.cta}>
+            <Link href="/meals/share">
+                Share your Favorite Recipe
+            </Link>
+          </p>
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={ meals }/>
+        </main>
+      </>
+    );
 }
