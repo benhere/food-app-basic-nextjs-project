80bdcdd49e682c80b54ade7ccfb11a548e41cc69
benhere <benjamin.oldfriend@gmail.com>
2024-03-01 17:39:04 +0530

    basic-nextjs-app_1.6_image_minor_issue_fixed

--- src/app/components/meals/meals-grid.js
+++ src/app/components/meals/meals-grid.js
@@ -2,11 +2,18 @@ import MealItem from './meal-item';
 import classes from './meals-grid.module.css'
 
 export default function MealsGrid({ meals }){
+  // console.log(...meals);
     return (
       <ul className={classes.meals}>
         {meals.map((meal) => (
           <li key={meal.id}>
-            <MealItem {...meals}/>
+            <MealItem
+              title={meal.title}
+              slug={meal.slug}
+              image={meal.image}
+              summary={meal.summary}
+              creator={meal.creator}
+            />
           </li>
         ))}
       </ul>
--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -7,6 +7,7 @@ export default async function MealsPage(){
     // console.log('Executing meals page...');
 
     const meals = await getMeals();
+    // console.log(meals);
     return (
       <>
         <header className={classes.header}>
